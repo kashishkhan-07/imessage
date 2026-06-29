ecd00371b9d4bb94cd768e8283ecd339c4fe85cf
Kashish Khan <kashishkhan@gmail.com>
2026-06-29 12:47:08 +0530

    Import cron job module in server.js for scheduled tasks

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,7 @@ import {clerkMiddleware} from "@clerk/express"
 
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js"
 
 const app = express();
 
