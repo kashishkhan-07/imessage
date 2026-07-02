c0f39ab30f73d01b93b0050bdee61fb168101e80
Kashish Khan <kashishkhan@gmail.com>
2026-07-02 12:54:47 +0530

    Add Clerk webhook handler for user events and integrate with server

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,9 @@ import {clerkMiddleware} from "@clerk/express"
 
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
-import job from "./lib/cron.js"
+import job from "./lib/cron.js";
+
+import clerkwebhook from "./webhooks/clerk.webhook.js";
 
 const app = express();
 
@@ -17,6 +19,9 @@ const PORT = process.env.PORT;
 const FRONTEND_URL=process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
+
+//it's imp that you don't parse the webhook event data,it should be in raw format
+app.use("/api/webhooks/clerk",express.raw({type:"application/json"}), clerkwebhook);
 
 app.use(express.json());
 app.use(cors({origin:FRONTEND_URL,credentials:true}));
